8e0ee9d5086e691ca650841b3e8e352851db3348
Zaid _ <117419326+x72x@users.noreply.github.com>
2023-09-16 01:34:43 +0300

    Create timeout_example.py

---
+++ examples/timeout_example.py
@@ -0,0 +1,25 @@
+import asyncio
+from telebot import async_telebot, types
+from telemod import Listener, TimeOut
+
+bot = async_telebot.AsyncTeleBot(token="")
+loop = asyncio.get_event_loop()
+
+listener = Listener(bot=bot, loop=loop)
+
+@bot.message_handler(commands=["start"])
+async def start_handler(message: types.Message):
+    try:
+        msg = await listener.listen_to(message, "What's your name?", timeout=10)
+    except TimeOut:
+        msg = None
+        await bot.reply_to(message, "Time Out")
+    if msg:
+        return await bot.reply_to(msg, f"Hi {msg.text}")
+
+async def main():
+    print((await bot.get_me()).first_name)
+    await listener.start()
+    await bot.infinity_polling(skip_pending=True)
+
+loop.run_until_complete(main())
