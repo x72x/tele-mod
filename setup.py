49a51c68882044c8684294e956938fdc3cbc8d71
Zaid _ <117419326+x72x@users.noreply.github.com>
2023-09-16 01:51:04 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.1'
+DESCRIPTION = 'Telebot listener'
+
+# Setting up
+setup(
+    name="tele-mod",
+    version=VERSION,
+    author="ZAID",
+    author_email="y8838@hotmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['asyncio', 'pytelegrambotapi'],
+    keywords=['python', 'pytelegrambotapi', 'telegram', 'telebot'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires="~=3.6",
+)
